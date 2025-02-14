41da919cf29b79834569e8d2622e76d95e267575
Bobby <bobur0218programmer@gmail.com>
2025-02-14 17:27:09 +0500

    feat: like state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,27 +8,28 @@ export default function App() {
   const [page, setPage] = useState(1);
   const [lang, setLang] = useState<Lang>("en");
   const [seed, setSeed] = useState(42);
+  const [like, setLike] = useState(3.5);
   const [loading, setLoading] = useState(false);
   const observer = useRef<IntersectionObserver | null>(null);
   const lastBookElementRef = useRef<HTMLDivElement | null>(null);
 
-  const fetchBooks = useCallback(
-    async (pageNum: number, reset: boolean = false) => {
-      try {
-        setLoading(true);
-        const res = await fetch(
-          `${BACKEND_URL}?page=${pageNum}&lang=${lang}&seed=${seed}`
-        );
-        const data = (await res.json()) as Book[];
-        setBooks((prevBooks) => (reset ? data : [...prevBooks, ...data]));
-      } catch (err) {
-        console.error("Error with fetching books: ", err);
-      } finally {
-        setLoading(false);
-      }
-    },
-    [lang, seed]
-  );
+  // const fetchBooks = useCallback(
+  //   async (pageNum: number, reset: boolean = false) => {
+  //     try {
+  //       setLoading(true);
+  //       const res = await fetch(
+  //         `${BACKEND_URL}?page=${pageNum}&lang=${lang}&seed=${seed}&like=${like}`
+  //       );
+  //       const data = (await res.json()) as Book[];
+  //       setBooks((prevBooks) => (reset ? data : [...prevBooks, ...data]));
+  //     } catch (err) {
+  //       console.error("Error with fetching books: ", err);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   },
+  //   [lang, seed, like]
+  // )
 
   const handleLanguageChange = async (newLang: Lang) => {
     setLang(newLang);
@@ -36,9 +37,11 @@ export default function App() {
 
     try {
       const booksPromises = Array.from({ length: page }, (_, i) =>
-        fetch(`${BACKEND_URL}?page=${i + 1}&lang=${newLang}&seed=${seed}`).then(
-          (res) => res.json()
-        )
+        fetch(
+          `${BACKEND_URL}?page=${
+            i + 1
+          }&lang=${newLang}&seed=${seed}&like=${like}`
+        ).then((res) => res.json())
       );
 
       const allBooksData = await Promise.all(booksPromises);
@@ -58,16 +61,40 @@ export default function App() {
 
     try {
       const booksPromises = Array.from({ length: page }, (_, i) =>
-        fetch(`${BACKEND_URL}?page=${i + 1}&lang=${lang}&seed=${newSeed}`).then(
-          (res) => res.json()
-        )
+        fetch(
+          `${BACKEND_URL}?page=${
+            i + 1
+          }&lang=${lang}&seed=${newSeed}&like=${like}`
+        ).then((res) => res.json())
       );
 
       const allBooksData = await Promise.all(booksPromises);
       const flattenedBooks = allBooksData.flat() as Book[];
       setBooks(flattenedBooks);
     } catch (err) {
-      console.error("Error fetching books fow new language:", err);
+      console.error("Error fetching books fow new seed:", err);
+      setBooks([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleLikeChange = async (newLike: number) => {
+    setLike(newLike);
+    setLoading(true);
+
+    try {
+      const booksPromises = Array.from({ length: page }, (_, i) =>
+        fetch(
+          `${BACKEND_URL}?page=${i + 1}&lang=${lang}&seed=${seed}&like=${like}`
+        ).then((res) => res.json())
+      );
+
+      const allBooksData = await Promise.all(booksPromises);
+      const flattenedBooks = allBooksData.flat() as Book[];
+      setBooks(flattenedBooks);
+    } catch (err) {
+      console.error("Error fetching books fow new like:", err);
       setBooks([]);
     } finally {
       setLoading(false);
@@ -75,8 +102,23 @@ export default function App() {
   };
 
   useEffect(() => {
+    const fetchBooks = async (pageNum: number, reset: boolean = false) => {
+      try {
+        setLoading(true);
+        const res = await fetch(
+          `${BACKEND_URL}?page=${pageNum}&lang=${lang}&seed=${seed}&like=${like}`
+        );
+        const data = (await res.json()) as Book[];
+        setBooks((prevBooks) => (reset ? data : [...prevBooks, ...data]));
+      } catch (err) {
+        console.error("Error with fetching books: ", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     fetchBooks(page);
-  }, [page, fetchBooks]);
+  }, [page]);
 
   const lastBookRef = useCallback(
     (node: HTMLDivElement) => {
@@ -131,6 +173,17 @@ export default function App() {
             className="p-2 border rounded"
             value={seed}
             onChange={(e) => handleSeedChange(parseInt(e.target.value))}
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="like">Avg. Like: 3.5</label>
+          <input
+            id="like"
+            type="number"
+            className="p-2 border rounded"
+            value={like}
+            onChange={(e) => handleLikeChange(parseFloat(e.target.value))}
+            step="0.1"
           />
         </div>
       </nav>
